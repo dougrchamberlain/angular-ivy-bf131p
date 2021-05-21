d40e037f46501b9ff7ae7bbabcbe17bd449d4e71
dougrchamberlain <d.chamberlain@nextech.com>
2021-05-21 13:04:39 -0400

    3 files added

---
+++ src/app/medications-container/medications-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-medications-container',
+  templateUrl: './medications-container.component.html',
+  styleUrls: ['./medications-container.component.css']
+})
+export class MedicationsContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
